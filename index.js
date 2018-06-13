eb16f8d5daaaea5982f5263542648bfdc507a79d
poom1450 <poom1450@gmail.com>
2018-06-14 01:30:23 +0800

    sadas
    sadad

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 3000;
+var app = require('express')();
+var http = require('http').Server(app);
 
-app.use(express.static(__dirname + "/public"));
-
-app.get('/:date',function(req,res){
-  var input = decodeURI(req.params.date);
-  var isNumber = /^\d+$/.test(input);
-  var unix, string;
-  if(isNumber){
-    var t = new Date(parseInt(input));
-    string = t.toUTCString();
-    unix = input;
-  } else {
-    unix = Date.parse(input) || "null";
-    string= Date.parse(input) ? input : "null";
-  }
-
-  var output = {
-    string: string,
-    unix:unix,
-  };
-
-  res.json(output)
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
 });
 
-app.listen(PORT,function(){
-  console.log('server successfully started on port '+PORT);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
+    
